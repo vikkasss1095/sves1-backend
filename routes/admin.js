1f6412941f1d96468c4c1cf1ee488edc9747acf0
Vikas <vikasthavaliya@gmail.com>
2026-05-05 23:41:05 +0530

    fix: route order for vendor details API

--- routes/admin.js
+++ routes/admin.js
@@ -20,15 +20,21 @@ const { isAdmin } = require('../middleware/roleMiddleware');
 router.use(protect, isAdmin);
 
 router.get('/dashboard', getDashboardStats);
+
+// 🔥 FIXED ORDER
+router.get('/vendors/:id', getSingleVendor);
 router.get('/vendors', getAllVendors);
+
 router.put('/vendors/:id/approve', approveVendor);
 router.put('/vendors/:id/reject', rejectVendor);
 router.delete('/vendors/:id', deleteVendor);
+
 router.get('/evaluation', getVendorRankings);
 router.post('/ratings', rateVendor);
+
 router.get('/documents', getAllDocuments);
 router.put('/documents/:id', reviewDocument);
+
 router.get('/payments', getAllPayments);
 router.put('/payments/:id/approve', approvePayment);
-router.get('/vendors/:id', getSingleVendor); 
 module.exports = router;
